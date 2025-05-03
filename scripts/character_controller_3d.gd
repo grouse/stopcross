caca5b1d09e4d490f84708ee285c93b16bca8caf
Jesper Stefansson <jesper.stefansson@gmail.com>
2025-05-03 15:14:09 +0200

    wip gravity and terminal velocity support for character controller, accumulating into an external velocity component

--- scripts/character_controller_3d.gd
+++ scripts/character_controller_3d.gd
@@ -7,11 +7,17 @@ extends CharacterBody3D
 @export_range(0, 100, 0.01, "suffix:s") var time_to_turn_90      : float = 0.05
 @export_range(0, 100, 0.01, "suffix:s") var time_to_stop_reverse : float = 0.15
 
+@export_group("Falling")
+@export_range(0, 100, 0.01) var gravity_scale : float = 1
+@export_range(0, 1000, 0.01, "suffix:m/s") var terminal_velocity : float = 50
+
 @export_group("Debug")
 @export var draw_movement_vectors : bool = false
 
 @onready var acceleration  = max_speed / time_to_max_speed
 @onready var deceleration  = max_speed / time_to_stop;
+
+var external_velocity : Vector3 = Vector3.ZERO
 
 var turn_rate_rad     = 10000
 var reverse_decel;
@@ -30,15 +36,30 @@ func reject(a : Vector3, b : Vector3) -> Vector3:
 	return a-b*(a.dot(b)/b.dot(b))
 
 func _physics_process(delta: float) -> void:
+	var gravity_dir = get_gravity().normalized()
+	var gravity_strength = get_gravity().length() * gravity_scale
+
 	var dir   = velocity.normalized()
 	var t_dir = input_direction.normalized()
+	t_dir = t_dir - t_dir.dot(gravity_dir)*gravity_dir
 
-	var accel = acceleration
+	var movement_velocity = velocity - velocity.dot(gravity_dir)*gravity_dir
+
+	var accel = acceleration if is_on_floor() else 0
 	var friction = 0
 
-	if velocity.length_squared() > 0.0001:
+	if not is_on_floor(): 
+		external_velocity += gravity_dir*gravity_strength*delta
+		var falling_component = external_velocity.dot(gravity_dir)*gravity_dir
+		if falling_component.length() > terminal_velocity:
+			external_velocity -= falling_component
+			external_velocity += gravity_dir*terminal_velocity
+	else:
+		external_velocity -= external_velocity.dot(gravity_dir)*gravity_dir
+
+	if movement_velocity.length_squared() > 0.0001:
 		if t_dir.length_squared() <= 0.0001:
-			friction = deceleration
+			if is_on_floor(): friction = deceleration
 		else:
 			var angle = dir.angle_to(t_dir)
 			var max_angle_change = turn_rate_rad*delta
@@ -49,25 +70,34 @@ func _physics_process(delta: float) -> void:
 			elif angle > max_angle_change:
 				var axis = dir.cross(t_dir).normalized()
 				if axis.length_squared() > 0.0001:
-					velocity = dir.rotated(axis, max_angle_change)*velocity.length()
-					dir = velocity.normalized()
+					movement_velocity = dir.rotated(axis, max_angle_change)*movement_velocity.length()
+					dir = movement_velocity.normalized()
 			else:
-				velocity = t_dir*velocity.length()
+				movement_velocity = t_dir*movement_velocity.length()
 				dir = t_dir
 	else:
 		dir = t_dir
 
-
-	var speed = velocity.length()
+	var speed = movement_velocity.length()
 	if t_dir.length_squared() > 0:
 		speed = min(speed+accel*delta, max_speed)
 	else:
 		speed = max(speed-friction*delta, 0)
 
 	if speed > 0 and dir.length_squared() > 0.0001:
-		velocity = dir*speed
+		movement_velocity = dir*speed
 	else:
-		velocity = Vector3.ZERO
+		movement_velocity = Vector3.ZERO
+
+	velocity = movement_velocity + external_velocity
+
+	if t_dir.length_squared() > 0:
+		$pivot.basis = Basis(t_dir, Vector3.UP, t_dir.cross(Vector3.UP).normalized())
+
+	var was_on_floor = is_on_floor()
+	var old_transform = global_transform
+
+	move_and_slide()
 
 	if draw_movement_vectors:
 		DebugDraw3D.draw_arrow(global_position, global_position+t_dir*acceleration, Color.RED, 0.1)
@@ -75,10 +105,12 @@ func _physics_process(delta: float) -> void:
 		DebugDraw3D.draw_arrow(global_position, global_position+velocity, Color.GREEN, 0.1)
 		DebugDraw3D.draw_text(global_position+Vector3.FORWARD+Vector3.UP, "%.2f" % velocity.length(), 64)
 
-	if t_dir.length_squared() > 0:
-		$pivot.basis = Basis(t_dir, Vector3.UP, t_dir.cross(Vector3.UP).normalized())
+		if is_on_floor() and !was_on_floor:
+			DebugDraw3D.draw_sphere(old_transform.origin, 0.5, Color.GREEN, 1)
+		elif !is_on_floor() and was_on_floor:
+			DebugDraw3D.draw_sphere(old_transform.origin, 0.5, Color.RED, 1)
 
-	move_and_slide()
+		DebugDraw3D.draw_sphere(old_transform.origin, 0.1, Color.WHITE, 1)
 
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
